62fa2222eb39fce3b7a0ffc88898b815069c1916
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-27 20:53:05 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{Fragment,useState,useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 function App() {
+
+  const [busqueda,guardarBusqueda]=useState({
+    ciudad:'',
+    pais:'',
+})
+
+const [consultar,guardarConsultar] = useState(false)
+const {ciudad, pais} = busqueda;
+const [resultado ,  guardarResultado]= useState({})
+const [error,guardarError]=useState(false)
+
+
+useEffect(()=>{
+  const consultarAPI= async()=>{
+    
+    if(consultar){  
+        
+      
+    const apiKey=`e2a5a5cbd628b1ebb94c1b1f59c41194`
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiKey}`
+    const respuesta= await fetch(url)
+
+    const resultado = await respuesta.json()
+    guardarResultado(resultado)
+    guardarConsultar(false)
+    
+
+    if(resultado.cod==="404"){
+      guardarError(true)
+    }else{guardarError(false)}
+  
+  }
+    
+  }
+  consultarAPI()
+
+  //eslint-disable-next-line
+},[consultar]);
+
+let componente;
+if(error){
+  componente= <Error mensaje = "No hay resultados"/>
+}else{
+  componente=  <Clima
+
+  resultado={resultado}
+ />
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Fragment>
+    <Header
+      titulo='Clima React APP'
+    />
+    <div className='contenedor-form'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col m6 s12'>
+            <Formulario
+              busqueda={busqueda}
+              guardarBusqueda={guardarBusqueda}
+              guardarConsultar={guardarConsultar}
+            />
+          </div>
+          <div className='col m6 s12'>
+           {componente}
+           
+          </div>
+        </div>
+      </div>
     </div>
+  </Fragment>
   );
 }
 
